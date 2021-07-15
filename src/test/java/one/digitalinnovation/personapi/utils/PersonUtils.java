218de46230619fd585b863c9579519b3257dc73e
Vinigrillo <98grillo@gmail.com>
2021-07-15 20:08:04 -0300

    Main

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -1,6 +1,6 @@
 package one.digitalinnovation.personapi.utils;
 
-import one.digitalinnovation.personapi.dto.request.Pers onDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
 
 import java.time.LocalDate;
@@ -8,11 +8,11 @@ import java.util.Collections;
 
 public class PersonUtils {
 
-    private static final String FIRST_NAME = "Rodrigo";
-    private static final String LAST_NAME = "Peleias";
-    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final String FIRST_NAME = "Vinicius";
+    private static final String LAST_NAME = "Grillo";
+    private static final String CPF_NUMBER = "123.456.789-99";
     private static final long PERSON_ID = 1L;
-    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+    public static final LocalDate BIRTH_DATE = LocalDate.of(1998, 04, 20);
 
     public static PersonDTO createFakeDTO() {
         return PersonDTO.builder()
